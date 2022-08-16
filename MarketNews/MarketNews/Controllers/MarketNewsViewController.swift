eaf5e24ea71de6054e7bcf2139ff351f663f84d7
Matthew Fraser <52222907+MatthewFraser22@users.noreply.github.com>
2022-08-16 17:33:28 -0400

    Add files via upload
    Setup basic storyboard layout
    Implemented Generic Networking service

---
+++ MarketNews/MarketNews/Controllers/MarketNewsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MarketNewsViewController.swift
+//  MarketNews
+//
+//  Created by Matthew Fraser on 2022-08-15.
+//
+
+import Foundation
+import UIKit
+
+class MarketNewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
